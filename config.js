c041387e67b4eb088d233836d26021fd9a58ad7b
tokuhomes <67738714+tokuhomes@users.noreply.github.com>
2022-04-21 16:31:16 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // @ts-check
 
 const config = {
-  endpoint: "https://iothubtest1cosmos1.documents.azure.com:443/",
-  key: "dKWT2m1Vz5eTzGj4E2tpg9UdiLmWFaRVxaqnkVWThCQm5GL0xbrsUQAbwGfcOYTSpY3LmzLDR7MaZaXanxRaKA==",
+  endpoint: "https://iothubtest1cosmosdb2.documents.azure.com:443/",
+  key: "BGoKkVXksyRlENbHHzf0LPHxBww5gDr2z5rA26s73rHLTGQcuKlUx68rbtt8fQ3P4NBu0tOm2ScgBKBaRCr1Fw==",
   databaseId: "Tasks",
   containerId: "Items",
   partitionKey: { kind: "Hash", paths: ["/category"] }
